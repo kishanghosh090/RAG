6cd1732aa6c35eed16c88b263baa0a80c9267d42
kishanghosh090 <kishanghosh090@gmail.com>
2026-07-02 20:41:53 +0530

    add server ollama code

--- fastaouollama/server.py
+++ fastaouollama/server.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from ollama import Client
 
 app = FastAPI()
-
+client = Client(
+    host="http://3.108.182.182:11434"
+)
 
 @app.get("/")
 def read_root():
     return {"msg":"hello form FASTAPI"}
+
+
+@app.post("/chat")
+def chat(
+        message: str = Body(...,description="the message")
+):
+    response = client.chat(model="qwen2.5:0.5b", messages=[
+        {"role": "user", "content": message}
+    ])
+    return response.message.content
