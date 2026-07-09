29cac348bb5c39d123ec234036a82fe05ba5d8d9
kishanghosh090 <kishanghosh090@gmail.com>
2026-07-09 21:34:27 +0530

    add mongo as check point

--- langgraph/chatcheckpoint.py
+++ langgraph/chatcheckpoint.py
@@ -4,6 +4,9 @@ from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
+from langgraph.checkpoint.mongodb import MongoDBSaver
+from pymongo import MongoClient  # <-- Imported MongoClient
+import os
 
 load_dotenv()
 llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
@@ -24,15 +27,35 @@ graph_builder = StateGraph(State)
 # build nodes
 graph_builder.add_node("chatbot", chatbot)
 
-
 # create edges
-
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
 
-graph = graph_builder.compile()
+def compile_graph_with_checkpointer():
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    
+    checkpointer = MongoDBSaver(client, db_name="lg")
+    
+    # 3. Compile and return
+    return graph_builder.compile(checkpointer=checkpointer)
 
-updated_state = graph.invoke(State({"messages": "hi my name is kishan rana ghosh"}))
-print(updated_state)
 
+graph_with_check_point = compile_graph_with_checkpointer()        
+
+config = {
+    "configurable": {
+        "thread_id": "kishan"
+    }
+}
+
+# First run: State initialization
+updated_state = graph_with_check_point.invoke(
+    {"messages": [{"role": "user", "content": "what is my name?"}]}, 
+    config
+)
+print("--- FIRST RESPONSE ---")
+print(updated_state["messages"][-1].content)
+
+
+# 
