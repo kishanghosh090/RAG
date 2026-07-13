f1057180716b3408bf82885b72af13c520823e13
kishanghosh090 <kishanghosh090@gmail.com>
2026-07-13 20:58:04 +0530

    init mcp server

---
+++ MCPPIYUSHSIR/index.ts
@@ -0,0 +1,53 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { string, z } from "zod";
+
+const server = new McpServer({
+  name: "chai",
+  version: "1.0.0",
+});
+
+async function getWeatherByCity(city: string) {
+
+  city = city.toLowerCase();
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=${city}`,
+  );
+  if (!response.ok) {
+    throw new Error(
+      `Weather API request failed with status ${response.status}`,
+    );
+  }
+  const data = await response.json();
+  return data;
+}
+server.registerTool(
+  "get_weather",
+  {
+    description:
+      "Fetch live regional forecasts from the US National Weather Service.",
+    inputSchema: {
+      city: z.string(),
+    },
+  },
+  async ({ city }) => {
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(await getWeatherByCity(city)),
+        },
+      ],
+    };
+  },
+);
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+main().catch((err) => {
+  console.error("McpServer failed to initiate:", err);
+  process.exit(1);
+});
